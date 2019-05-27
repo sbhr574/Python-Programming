27558087f4ad8a281bf3553819c8ca8b935cae43
Subhajit Roy <sbhr574@gmail.com>
2019-05-27 23:48:33 +0530

    Creating Python Repo

---
+++ PythonProgramming/PythonBasicProgramming/DataTypes.py
@@ -0,0 +1,19 @@
+#Ex:1
+name = input('What is your name\n')
+name1 = input('which color do you like\n')
+print(name +' likes '+ name1)
+
+#Ex:2
+i = 32
+print(i)
+i = 32.1
+print(i)
+i = 'test'
+print(i)
+
+#Ex:3(Same memory Location)
+x= 50
+y=x
+print (x)#50
+print(y)#50
+
---
+++ PythonProgramming/PythonBasicProgramming/PythonString.py
@@ -0,0 +1,3 @@
+#Ex:1
+string = 'Python\'s learning'
+print(string)
