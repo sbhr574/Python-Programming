05234b4487998496c361d97cbaf22731b382e38a
sbhr574 <sbhr574@gmail.com>
2021-03-14 23:25:43 +0530

    Super keyword

--- PythonProgramming/OOPS_Concept/Super_KeyWord.py
+++ PythonProgramming/OOPS_Concept/Super_KeyWord.py
@@ -1,3 +1,4 @@
+# Example 1
 class rik:
     def roy(self):
         print("Rik khub valo.")
@@ -8,8 +9,26 @@ class rik:
 class rai(rik):
     def roy(self):
         super().roy()
+        super().bye()
         super(rai, self).bye()
         print("rai khub valo.")
 
 obj = rai()
 obj.roy()
+
+# Example 2
+class test:
+    def __init__(self,name):
+        print("from cons "+name)
+
+    def m1(self):
+        print("from test")
+
+class test1(test):
+    def __init__(self):
+        super().__init__('test class')
+    def m2(self):
+        print("from test1")
+
+xy = test1()
+xy.m2()
