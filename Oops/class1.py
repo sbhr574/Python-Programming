0c1332ca849a3d35ef0b9011e9b95d7396f70c4c
unknown <sbhr574@gmail.com>
2026-03-08 22:21:21 +0530

    Oops file

---
+++ Oops/class1.py
@@ -0,0 +1,22 @@
+class Place:
+    country = ""
+
+    def __init__(self, city, state):
+        self.city = city
+        self.state = state
+
+    def mango(self):
+        print(f"{self.city} best for mango. WHich is based on {Place.country}.")
+
+Place.country = "India"
+
+place1_obj = Place("Malda", "WB")
+
+#outside class
+def mango1(area):
+        print(f"{area} best for man.")
+    
+place1_obj.mango()
+mango1("Kaj")
+
+
